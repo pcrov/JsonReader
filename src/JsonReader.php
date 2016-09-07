319436ea271093d5fca37dd0f66119c9a3fba19b
Paul Crovella <paul.crovella@gmail.com>
2016-09-07 04:30:58 -0700

    Fix missed phpdoc in last commit.

--- src/JsonReader.php
+++ src/JsonReader.php
@@ -205,7 +205,7 @@ class JsonReader
      *
      * @param string|resource $file URI or file handle.
      * @return void
-     * @throws \InvalidArgumentException if a given resource is not a valid stream.
+     * @throws InvalidArgumentException if a given resource is not a valid stream.
      * @throws IOException if a given stream resource is not readable.
      */
     public function open($file)
