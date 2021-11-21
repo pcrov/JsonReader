0fed6671d7ac7b8b10f40fb3a987aa57eb6d73aa
Paul Crovella <paul.crovella@gmail.com>
2021-11-20 22:34:16 -0800

    Yay, const visibility

--- src/Parser/Tokenizer.php
+++ src/Parser/Tokenizer.php
@@ -4,18 +4,18 @@ namespace pcrov\JsonReader\Parser;
 
 interface Tokenizer
 {
-    const T_STRING = "T_STRING";
-    const T_NUMBER = "T_NUMBER";
-    const T_TRUE = "T_TRUE";
-    const T_FALSE = "T_FALSE";
-    const T_NULL = "T_NULL";
-    const T_COLON = "T_COLON";
-    const T_COMMA = "T_COMMA";
-    const T_BEGIN_ARRAY = "T_BEGIN_ARRAY";
-    const T_END_ARRAY = "T_END_ARRAY";
-    const T_BEGIN_OBJECT = "T_BEGIN_OBJECT";
-    const T_END_OBJECT = "T_END_OBJECT";
-    const T_EOF = "T_EOF";
+    public const T_STRING = "T_STRING";
+    public const T_NUMBER = "T_NUMBER";
+    public const T_TRUE = "T_TRUE";
+    public const T_FALSE = "T_FALSE";
+    public const T_NULL = "T_NULL";
+    public const T_COLON = "T_COLON";
+    public const T_COMMA = "T_COMMA";
+    public const T_BEGIN_ARRAY = "T_BEGIN_ARRAY";
+    public const T_END_ARRAY = "T_END_ARRAY";
+    public const T_BEGIN_OBJECT = "T_BEGIN_OBJECT";
+    public const T_END_OBJECT = "T_END_OBJECT";
+    public const T_EOF = "T_EOF";
 
     /**
      * @return array Tuples in the form of [string type, mixed value, int line]
